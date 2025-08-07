6c19c08c01ba844b3e55209bcd52c19358b9f9c3
Muhammad <imuhammadsani08@gmail.com>
2025-08-07 18:16:08 +0100

    Events

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,36 @@
-import { View,Text } from "react-native";
+import { events } from "@/assets/local-data/events";
+import { Text, View, FlatList, Image, } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+function Separator () {
+    return (
+        <View style={{height:16, backgroundColor:"transparent"}}></View>
+    )
+}
+
 
 export default function Index () {
     return (
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-            <Text>Home screen on tabs</Text>
-        </View>
+
+       <SafeAreaProvider>
+        <SafeAreaView style={{
+            flex: 1
+        }}>
+           <FlatList
+           data={events}
+           renderItem={({item}) => {
+            return (
+                <View>
+                    <Image
+                    source={{uri: item.bannerUrl}}
+                    alt="event photo"
+                    style={{width:600,height:400,resizeMode:"cover"}}/>
+                    <Text className="text-xs font-bold">{item.title}</Text>
+                </View>
+            )
+           }}
+           keyExtractor={(item) => item.id}/>
+        </SafeAreaView>
+       </SafeAreaProvider>
     )
 }
